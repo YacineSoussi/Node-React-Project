ba0339e546198ad17b8cbf6c32850f19a1416795
ali.raid <ali.raid@mangopay.com>
2022-07-06 18:58:05 +0200

    backend: resolve conflits

--- server/models/mongo/db.js
+++ server/models/mongo/db.js
@@ -1,25 +1,12 @@
-<<<<<<< HEAD
-// Connection a mongo
-var mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGO_URL).then(() => {
-    console.log('Connected to MongoDB');
-}
-).catch(err => {
-    console.error('Error connecting to MongoDB: ' + err.message);
-}
-);
-=======
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.error("Error connecting to MongoDB: ", err.message);
-  });
->>>>>>> c040b16 (backend : add user endpoints)
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.error("Error connecting to MongoDB: ", err.message);
+    });
 
 module.exports = mongoose;
