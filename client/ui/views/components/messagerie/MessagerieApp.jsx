712672878a8f3b30d837684899e35f6857322f55
Pereira Júlio <julioocesarr@outlook.fr>
2022-07-27 21:34:35 +0200

    Indentation + refactoring

--- client/ui/views/components/messagerie/MessagerieApp.jsx
+++ client/ui/views/components/messagerie/MessagerieApp.jsx
@@ -1,20 +1,15 @@
-import {Route, Switch} from 'react-router-dom';
 import { getAccessToken, getUserData } from '../../../../adapters/CookiesAppStorage';
 import { useEffect, useState } from 'react';
-import {useMustBeAuthenticateGuard} from '../../../common/Hooks/useMustBeAuthenticateGuard';
+
 import App from './Conversations/App';
-import Blank from './BlocMessage/Blank';
 import React from 'react';
 import Right from './BlocMessage/Right';
-import Modal from './Modal/Modal';
-
+import {useMustBeAuthenticateGuard} from '../../../common/Hooks/useMustBeAuthenticateGuard';
 
 export const MessagerieApp = () => {
-
     useMustBeAuthenticateGuard();
 
-    
-    const [authorLastMessage, setAuthorLastMessage] = useState(null); 
+    const [authorLastMessage, setAuthorLastMessage] = useState(null);
     const [conversations, setConversations] = useState([]);
     const [selectedConversationId, setSelectedConversationId] = useState(null);
     const [selectedConversation, setSelectedConversation] = useState(null);
@@ -22,26 +17,22 @@ export const MessagerieApp = () => {
     const [user, setUser] = useState(null);
     const [updatedConversation, setUpdatedConversation] = useState(null);
     const [amis, setAmis] = useState([]);
-
     const [isOpen, setIsOpen] = useState(false);
 
-
-    
     // On remet à jour la liste des conversations lorsqu'on modifie une conversation
     useEffect(() => {
         fetchConversations();
         setUser(getUserData());
     }, [updatedConversation]);
 
-        // On met a jour la conversation selectionnée dès le chargement de la page avec la premiere
+    // On met a jour la conversation selectionnée dès le chargement de la page avec la premiere
     useEffect(() => {
-        
-            if (selectedConversationId) {
-                fetchSelectedConversation();
-            }
-        }, [selectedConversationId]);
+        if (selectedConversationId) {
+            fetchSelectedConversation();
+        }
+    }, [selectedConversationId]);
 
-        // Récupère les conversations de l'utilisateur connecté
+    // Récupère les conversations de l'utilisateur connecté
     const fetchConversations = async () => {
         const response = await fetch(`http://localhost:3000/myconversations/${getUserData().id}`, {
             method: 'GET',
@@ -49,53 +40,45 @@ export const MessagerieApp = () => {
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${getAccessToken()}`
             }
-        })
-            .then(response => response.json())
-            .then(data => {
-                // Trier les conversations par date de dernier message
-                data.sort((a, b) => {
+        }).then(response => response.json()).then(data => {
+            // Trier les conversations par date de dernier message
+            data.sort((a, b) => {
                     return new Date(b.updatedAt) - new Date(a.updatedAt);
-                }
-                );
-                
-                const conversations = data.map(conversation => {
+            });
 
+            const conversations = data.map(conversation => {
                     // Trier les messages de la conversation par createdAt
                     const sortedConversation = conversation.messages.sort((a, b) => {
                         return new Date(a.createdAt) - new Date(b.createdAt);
                     });
-                   
+
                     conversation.messages = sortedConversation;
                     let lastMessage = null;
-                
+
                 if (conversation.messages.length > 0) {
                     // On récupère ici le dernier elemement du tableau pour le mettre dans lastMessage
                     lastMessage = conversation.messages[conversation.messages.length-1];
-                    }
-                    
-                
+                }
+
                 return {
                     ...conversation,
                     lastMessage
                 }
-            }
-            );
-            
-               setConversations(conversations);
+            });
 
-               if (selectedConversationId === null) {
-                   setSelectedConversationId(conversations[0].id);
-               }
-              
+            setConversations(conversations);
 
-            }).catch(error => {
-                console.error(error);
+            if (selectedConversationId === null) {
+                setSelectedConversationId(conversations[0].id);
             }
-            );
+        }).catch(error => {
+            console.error(error);
+        });
 
         return response;
     }
-        // Récupère la conversation selectionnée
+
+    // Récupère la conversation selectionnée
     const fetchSelectedConversation = async () => {
         const response = await fetch(`http://localhost:3000/conversations/${selectedConversationId}`, {
             method: 'GET',
@@ -103,35 +86,32 @@ export const MessagerieApp = () => {
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${getAccessToken()}`
             }
-        })
-        .then(response => response.json())
-        .then(data => {
+        }).then(response => response.json()).then(data => {
             // On trie les messages par date de creation pour eviter de changer l'ordre des messages quand ils sont modifié
             const sortedConversation = data.messages.sort((a, b) => {
                 return new Date(a.createdAt) - new Date(b.createdAt);
             });
-           
+
             data.messages = sortedConversation;
-            
+
             let lastMessage = null;
-                
-                if (data.messages.length > 0) {
-                    
-                     lastMessage = data.messages[data.messages.length-1];
-                  
-                }
-                const newConversation = {
-                    ...data,
-                    lastMessage
-                }
-                setSelectedConversation(newConversation);
-                setMessages(data.messages);
-               
-            })
-        .catch(error => {
-                console.error(error);
+
+            if (data.messages.length > 0) {
+                lastMessage = data.messages[data.messages.length-1];
             }
-            );
+
+            const newConversation = {
+                ...data,
+                lastMessage
+            }
+
+            setSelectedConversation(newConversation);
+            setMessages(data.messages);
+
+        }).catch(error => {
+            console.error(error);
+        });
+
         return response;
     }
 
@@ -143,36 +123,29 @@ export const MessagerieApp = () => {
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${getAccessToken()}`
             }
-        })
-            .then(response => response.json())
-            .then(data => {
-                setAmis(data);
-            }
-            ).catch(error => {
-                console.error(error);
-            }
-            );
+        }).then(response => response.json()).then(data => {
+            setAmis(data);
+        }).catch(error => {
+            console.error(error);
+        });
+
         return response;
     }
 
     // Permet de récuperer tous les participants qui ont une conversation avec l'user connecté
-    const participants = [];  
+    const participants = [];
     const getParticipants = (conversations) => {
-        
         conversations.forEach(conversation => {
-            
             conversation.participants.forEach(participant => {
                 if (participant.userId !== user.id) {
                     participants.push(participant);
                 }
             }
             );
-        }
-        );
-        
-         return participants;
-         
+        });
+        return participants;
     }
+
     // Met à jour la liste d'amis dès qu'une conversations est modifiée/crée
     useEffect(() => {
         fetchUsers();
@@ -182,40 +155,35 @@ export const MessagerieApp = () => {
     return (
         <div className="container py-5 px-4 message-container">
         <div className="row rounded-lg overflow-hidden shadow d-flex message-row">
+            <App
+                authorLastMessage={authorLastMessage}
+                setAuthorLastMessage={setAuthorLastMessage}
+                amis={amis}
+                isOpen={isOpen}
+                setIsOpen={setIsOpen}
+                setConversations={setConversations}
+                user={user}
+                selectedConversationId={selectedConversationId}
+                updateSelectedConversationId={setSelectedConversationId}
+                selectedConversation={selectedConversation}
+                conversations={conversations}
+                getParticipants={getParticipants}
+                participants={participants} />
 
-            <App 
-            authorLastMessage={authorLastMessage} 
-            setAuthorLastMessage={setAuthorLastMessage} 
-            amis={amis} 
-            isOpen={isOpen} 
-            setIsOpen={setIsOpen} 
-            setConversations={setConversations} 
-            user={user} 
-            selectedConversationId={selectedConversationId} 
-            updateSelectedConversationId={setSelectedConversationId} 
-            selectedConversation={selectedConversation}
-            conversations={conversations}
-            getParticipants={getParticipants}
-            participants={participants} />
-
-            <Right 
-            authorLastMessage={authorLastMessage} 
-            setAuthorLastMessage={setAuthorLastMessage} 
-            setUpdatedConversation={setUpdatedConversation} 
-            conversation={selectedConversation} 
-            setConversation={setSelectedConversation} 
-            user={user} 
-            messages={Messages} 
-            setMessages={setMessages} 
-            selectedConversationId={selectedConversationId}
-            conversations={conversations}
-            setConversations={setConversations}
+            <Right
+                authorLastMessage={authorLastMessage}
+                setAuthorLastMessage={setAuthorLastMessage}
+                setUpdatedConversation={setUpdatedConversation}
+                conversation={selectedConversation}
+                setConversation={setSelectedConversation}
+                user={user}
+                messages={Messages}
+                setMessages={setMessages}
+                selectedConversationId={selectedConversationId}
+                conversations={conversations}
+                setConversations={setConversations}
             />
-
         </div>
-
-       
     </div>
     );
 };
-
--- client/ui/views/components/messagerie/Modal/Modal.jsx
+++ client/ui/views/components/messagerie/Modal/Modal.jsx
@@ -1,77 +1,58 @@
 import React from "react";
-import styles from "./Modal.module.css";
 import {RiCloseLine}  from 'react-icons/ri';
 import { getAccessToken } from '../../../../../adapters/CookiesAppStorage';
+import styles from "./Modal.module.css";
 const Modal = (props) => {
 
-       //   Permet de créer une nouvelle conversation 
-
-  const fetchPostConversation = async (secondUserId) => {
-        const response = await fetch(`http://localhost:3000/conversations`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${getAccessToken()}`
-            },
-            body: JSON.stringify({
-               "secondUserId": secondUserId,
-               "firstUserId": props.user.id
-
-            })
+// Permet de créer une nouvelle conversation
+const fetchPostConversation = async (secondUserId) => {
+    const response = await fetch(`http://localhost:3000/conversations`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${getAccessToken()}`
+        },
+        body: JSON.stringify({
+            "secondUserId": secondUserId,
+            "firstUserId": props.user.id
         })
-        .then(response => response.json())
-        .then(data => {
-            props.updateSelectedConversationId(data.id);
-            props.setIsOpen(false);
+    })
+    .then(response => response.json())
+    .then(data => {
+        props.updateSelectedConversationId(data.id);
+        props.setIsOpen(false);
 
-            let conversations = props.conversations;
-            conversations.push(data);
-           
-        }
-        ).catch(error => {
-            console.error(error);
-        }
-        );
-        return response;
-    }
+        let conversations = props.conversations;
+        conversations.push(data);
 
-  return (
+    }).catch(error => {
+        console.error(error);
+    });
+    return response;
+}
+
+    return (
     <>
     <div className={styles.darkBG} onClick={() => props.setIsOpen(false)} />
-      <div className={styles.centered}>
-        <div className={styles.modal}>
-          <div className={styles.modalHeader}>
-            <h5 className={styles.heading}>Séléctionner un ami</h5>
-          </div>
-          <button className={styles.closeBtn} onClick={() => props.setIsOpen(false)}>
-            <RiCloseLine style={{ marginBottom: "-3px" }} />
-          </button>
-          <div className={styles.modalContent}>
-
-            {props.amis ? props.amis.map((ami, key) => {
-                return (
-                    <>
-                    <div onClick={() => fetchPostConversation(ami.id)} key={key} style={{cursor: "pointer"}}>{ami.lastName} {ami.firstName}</div>
-                    </>
-                );
-            }) : "Ooups vous n'avez pas d'amis"}
-            
-          </div>
-          {/* <div className={styles.modalActions}>
-            <div className={styles.actionsContainer}>
-              <button className={styles.deleteBtn} onClick={() => setIsOpen(false)}>
-                Delete
-              </button>
-              <button
-                className={styles.cancelBtn}
-                onClick={() => setIsOpen(false)}
-              >
-                Cancel
-              </button>
+        <div className={styles.centered}>
+            <div className={styles.modal}>
+                <div className={styles.modalHeader}>
+                    <h5 className={styles.heading}>Séléctionner un ami</h5>
+                </div>
+                <button className={styles.closeBtn} onClick={() => props.setIsOpen(false)}>
+                    <RiCloseLine style={{ marginBottom: "-3px" }} />
+                </button>
+                <div className={styles.modalContent}>
+                    {props.amis ? props.amis.map((ami, key) => {
+                        return (
+                            <>
+                                <div onClick={() => fetchPostConversation(ami.id)} key={key} style={{cursor: "pointer"}}>{ami.lastName} {ami.firstName}</div>
+                            </>
+                        );
+                    }) : "Ooups vous n'avez pas d'amis"}
+                </div>
             </div>
-          </div> */}
         </div>
-      </div>
     </>
     );
 };
