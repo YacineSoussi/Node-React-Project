142099cd9fa76007dbf881e8a6094dadc3dda868
Pereira Júlio <julioocesarr@outlook.fr>
2022-07-27 21:56:53 +0200

    Implement deletion of messages

--- client/ui/views/components/messagerie/BlocMessage/Input.jsx
+++ client/ui/views/components/messagerie/BlocMessage/Input.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
 import { getAccessToken, getUserData } from '../../../../../adapters/CookiesAppStorage';
-import { useEffect, useRef, useState } from 'react';
-import io from 'socket.io-client';
+import { useEffect, useRef } from 'react';
+
+import React from 'react';
 import socket from '../../../../common/config/socket.js'
 
 const Input = (props) => {
@@ -9,29 +9,28 @@ const Input = (props) => {
 
 const socketRef = useRef(null);
 
-useEffect(() => { 
+useEffect(() => {
     const userId = getUserData().id;
+
     // On récupère l'instance de connexion aux sockets
-     socketRef.current = socket;
-    // On enregistre l'id de l'utilisateur dans le socket auth 
-     socketRef.current.auth = { userId };
-    //  On se connecte au socket 
-     socketRef.current.connect();
-     
+    socketRef.current = socket;
+    // On enregistre l'id de l'utilisateur dans le socket auth
+    socketRef.current.auth = { userId };
+    //  On se connecte au socket
+    socketRef.current.connect();
+
 
     // À chaque fois qu'un message est envoyé à l'utilisateur connecté à qui est adressé le message, on récupère le message et on l'affiche dans la conversation
-      socketRef.current.on("private_message", ({conversationMAJ, author, data, allMessages }) => {
-    
+    socketRef.current.on("private_message", ({conversationMAJ, author, data, allMessages }) => {
         props.setUpdatedConversation(conversationMAJ);
         props.setAuthorLastMessage(author);
-    
-         props.setMessages([...allMessages, data]);
-      });
-      
-      socketRef.current.on("update_message", ({content, from, conversations, newConversation, to, conversationAMAJ, author }) => {
-        
-        // Ici on cherche la conversation qu'on a mis a jour et on la met a jour 
+        props.setMessages([...allMessages, data]);
+    });
+
+    socketRef.current.on("update_message", ({conversations, newConversation, conversationAMAJ, author }) => {
+        // Ici on cherche la conversation qu'on a mis a jour et on la met a jour
         conversations[conversationAMAJ] = newConversation;
+
         // On modifie le state de la conversation active pour changer l'affichage du dernier msg
         props.setConversation(newConversation);
         props.setMessages([...newConversation.messages]);
@@ -40,33 +39,46 @@ useEffect(() => {
         props.setAuthorLastMessage(author);
         // Pour pouvoir indiquer qu'actuellement il n'y a pas de message a modifier ou supprimer
         props.setMessageUpdate(null);
-      });
-    
+    });
+
+    socketRef.current.on("delete_message", ({conversationAMAJ, author, newConversation, conversations }) => {
+        // Ici on cherche la conversation qu'on a mis a jour et on la met a jour
+        conversations[conversationAMAJ] = newConversation;
+
+        // On modifie le state de la conversation active pour changer l'affichage du dernier msg
+        props.setConversation(newConversation);
+        props.setMessages([...newConversation.messages]);
+
+        props.setUpdatedConversation(newConversation);
+        props.setAuthorLastMessage(author);
+
+        // Pour pouvoir indiquer qu'actuellement il n'y a pas de message a modifier ou supprimer
+        props.setMessageUpdate(null);
+    });
+
     socketRef.current.on("connect_error", (err) => {
         if (err.message === "invalid username") {
-          console.log("invalid username");
+            console.log("invalid username");
         }
-      });
+    });
 
     return () => socketRef.current.close();
-
 }, []);
 
 
-    
-    const Submit = (e) => {
-        e.preventDefault();
-        const socket = socketRef.current;
-        
-        const destinataire = props.conversation.participants.filter(user => user.userId !== props.user.id)[0];
-        const content = e.target[0].value;
-        if(props.messageUpdate) {
-            fetchUpdateMessage(e, socket, destinataire.userId);
-            
-        } else {
-            fetchNewMessage(e, socket, destinataire.userId);
-        }
+
+const Submit = (e) => {
+    e.preventDefault();
+    const socket = socketRef.current;
+    const destinataire = props.conversation.participants.filter(user => user.userId !== props.user.id)[0];
+
+    if(props.messageUpdate) {
+        fetchUpdateMessage(e, socket, destinataire.userId);
+
+    } else {
+        fetchNewMessage(e, socket, destinataire.userId);
     }
+}
 
     // Permet de post un nouveau message
     const fetchNewMessage = async (e, socket, destinataire) => {
@@ -88,10 +100,11 @@ useEffect(() => {
         })
         .then(response => response.json())
         .then(data => {
-           
             const messages = props.messages;
+
             props.setMessages([...messages, data]);
             e.target[0].value = '';
+
             // On modifie ici la conversation active avec le nouveau message afin de modifier le state pour que le coté gauche réagisse
             const conversation = props.conversation;
             const conversationMAJ = {
@@ -107,7 +120,7 @@ useEffect(() => {
             props.setConversation(conversationMAJ);
             props.setUpdatedConversation(conversationMAJ);
             props.setAuthorLastMessage(userId);
-           
+
             // On envoi le message au destinataire dans l'event "private_message" qui est défini dans le fichier index.js
 
             socket.emit("private_message", {
@@ -118,7 +131,7 @@ useEffect(() => {
                 allMessages: props.messages,
                 data: data
             });
-            
+
         }
         ).catch(error => {
             console.error(error);
@@ -129,7 +142,6 @@ useEffect(() => {
 
     // Permet de mettre à jour un message
     const fetchUpdateMessage = async (e, socket, destinataire) => {
-        
         const message = e.target[0].value;
         const response = await fetch(`http://localhost:3000/messages/${props.messageUpdate.id}`, {
             method: 'PUT',
@@ -144,21 +156,21 @@ useEffect(() => {
         })
         .then(response => response.json())
         .then(data => {
-            // Ici on cherche le message qu'on est en train de modifier pour le mettre à jour 
+            // Ici on cherche le message qu'on est en train de modifier pour le mettre à jour
             const messages = props.messages;
             const index = messages.findIndex(message => message.id === data.id);
-            
+
             messages[index] = data;
             props.setMessages(messages);
             e.target[0].value = '';
 
-            
             // On regarde si le message qu'on modifie est le dernier message de la conversation pour le mettre à jour
             let lastMsg = props.conversation.lastMessage;
-            
+
             if(data.id === props.conversation.lastMessageId) {
                 lastMsg = data;
             }
+
             // On modifie ici la conversation active avec le nouveau message afin de modifier le state pour que le coté gauche réagisse
             const conversation = props.conversation;
             const conversationMAJ = {
@@ -170,13 +182,15 @@ useEffect(() => {
                 participants: conversation.participants,
                 createdAt: data.createdAt
             };
-    
-            // Ici on cherche la conversation qu'on a mis a jour et on la met a jour 
+
+            // Ici on cherche la conversation qu'on a mis a jour et on la met a jour
             const indexConversation = props.conversations.findIndex(conversation => conversation.id === conversationMAJ.id);
+
             props.conversations[indexConversation] = conversationMAJ;
-           
+
             // On modifie le state de la conversation active pour changer l'affichage du dernier msg
             props.setConversation(conversationMAJ);
+
             // Pour pouvoir indiquer qu'actuellement il n'y a pas de message a modifier ou supprimer
             props.setMessageUpdate(null);
 
@@ -188,19 +202,17 @@ useEffect(() => {
                 conversations: props.conversations,
                 author: props.user.id,
                 messages: messages
-            })
+            });
 
-        }
-        ).catch(error => {
+        }).catch(error => {
             console.error(error);
-        }
-        );
+        });
+
         return response;
     }
 
     // Permet de supprimer un message
     const fetchDeleteMessage = async () => {
-
         const response = await fetch(`http://localhost:3000/messages/${props.messageUpdate.id}`, {
             method: 'PUT',
             headers: {
@@ -214,77 +226,85 @@ useEffect(() => {
         })
         .then(response => response.json())
         .then(data => {
+            const destinataire = props.conversation.participants.filter(user => user.userId !== props.user.id)[0];
 
-            // Ici on cherche le message qu'on est en train de modifier pour le mettre à jour 
+            // Ici on cherche le message qu'on est en train de modifier pour le mettre à jour
             const messages = props.messages;
             const index = messages.findIndex(message => message.id === data.id);
-            
+
             messages[index] = data;
             props.setMessages(messages);
 
-            
             // On regarde si le message qu'on modifie est le dernier message de la conversation pour le mettre à jour
             let lastMsg = props.conversation.lastMessage;
-            
+
             if(data.id === props.conversation.lastMessageId) {
                 lastMsg = data;
             }
+
             // On modifie ici la conversation active avec le nouveau message afin de modifier le state pour que le coté gauche réagisse
             const conversation = props.conversation;
             const conversationMAJ = {
                 lastMessage: lastMsg,
-                messages: [...conversation.messages],
+                messages: [...messages],
                 id: conversation.id,
                 updatedAt: data.updatedAt,
                 lastMessageId: lastMsg.id,
                 participants: conversation.participants,
                 createdAt: data.createdAt
             };
-    
-            // Ici on cherche la conversation qu'on a mis a jour et on la met a jour 
+
+            // Ici on cherche la conversation qu'on a mis a jour et on la met a jour
             const indexConversation = props.conversations.findIndex(conversation => conversation.id === conversationMAJ.id);
+
             props.conversations[indexConversation] = conversationMAJ;
-           
+
             // On modifie le state de la conversation active pour changer l'affichage du dernier msg
             props.setConversation(conversationMAJ);
             // Pour pouvoir indiquer qu'actuellement il n'y a pas de message a modifier ou supprimer
             props.setMessageUpdate(null);
-        }
-        ).catch(error => {
+
+            socket.emit("delete_message", {
+                content: "Ce message a été supprimé",
+                to: destinataire.userId,
+                conversationAMAJ: indexConversation,
+                newConversation: conversationMAJ,
+                conversations: props.conversations,
+                author: props.user.id,
+                messages: messages
+            });
+        }).catch(error => {
             console.error(error);
-        }
-        );
+        });
         return response;
-     }
-    
+    }
+
     // Ref qui va permettre de scroll automatiquement au dernier message
     const updateMessageRef = useRef(null);
-        
+
     // A chaque fois qu'un message est cliqué et que le state est modifié, on rempli l'input avec le message
-        useEffect(() => {
-            if (props.messageUpdate && props.messageUpdate.state !== undefined && props.messageUpdate.state !== "delete") {
+    useEffect(() => {
+        if (props.messageUpdate && props.messageUpdate.state !== undefined && props.messageUpdate.state !== "delete") {
             updateMessageRef.current.value = props.messageUpdate.content;
-            } else if(props.messageUpdate && props.messageUpdate.state === "delete") {
-                fetchDeleteMessage();
-            }
-            
+        } else if(props.messageUpdate && props.messageUpdate.state === "delete") {
+            fetchDeleteMessage();
+        }
     }, [props.messageUpdate]);
 
     return (
         <>
-        
-        <form onSubmit={Submit} action="#" className="bg-light mb-auto form-input">
-            <div className="input-group">
-                <input ref={updateMessageRef} type="text" placeholder="Envoyer un message"
-                    aria-describedby="button-addon2"
-                    className="form-control rounded-0 border-0 py-4 bg-light"/>
-                <div className="input-group-append">
-                    <button id="button-addon2" type="submit"className="btn btn-link">
-                        <i className="fa fa-paper-plane"></i>
-                    </button>
+            <form onSubmit={Submit} action="#" className="bg-light mb-auto form-input">
+                <div className="input-group">
+                    <input ref={updateMessageRef} type="text" placeholder="Envoyer un message"
+                        aria-describedby="button-addon2"
+                        className="form-control rounded-0 border-0 py-4 bg-light"/>
+                    <div className="input-group-append">
+                        <button id="button-addon2" type="submit"className="btn btn-link">
+                            <i className="fa fa-paper-plane"></i>
+                        </button>
+                    </div>
                 </div>
-            </div>
-        </form>
+            </form>
         </>
     )
 };
