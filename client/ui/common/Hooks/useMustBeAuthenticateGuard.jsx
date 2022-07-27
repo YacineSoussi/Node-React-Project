d92ff0186248a49445509face3068dbd5e164bdd
Pereira Júlio <julioocesarr@outlook.fr>
2022-07-27 20:39:36 +0200

    Refactoring + indentations

--- client/ui/common/Hooks/useMustBeAuthenticateGuard.jsx
+++ client/ui/common/Hooks/useMustBeAuthenticateGuard.jsx
@@ -1,15 +1,14 @@
+import {appRouteNames} from '../../../configuration/appRoutes';
+import { isUserAuthenticated } from '../../../adapters/AuthenticationService';
 import { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import { isUserAuthenticated } from '../../../adapters/AuthenticationService';
-import {appRouteNames} from '../../../configuration/appRoutes';
 
 export function useMustBeAuthenticateGuard (){
     const history = useHistory();
 
     useEffect(() => {
-        if(! isUserAuthenticated()){
+        if(!isUserAuthenticated()){
             history.push(appRouteNames.LOGIN_USER);
         }
-    }, [history])
-
+    }, [history]);
 }
--- client/ui/common/PageHeader.jsx
+++ client/ui/common/PageHeader.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const PageHeader = ({children}) => {
-    return <h4 style={{ color: 'var(--brown-4)' }}>
-    {children}
-        </h4>
+    return (
+        <h4 style={{ color: 'var(--brown-4)' }}>{children}</h4>
+    )
 }
